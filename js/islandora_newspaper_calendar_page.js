7c760395ea6f7b2cbc00dbf223b7ac7f79ac64df
ekennedy <evkennedy@upei.ca>
2014-04-10 13:09:45 -0300

    Each selected year now generates it's own page/URL.
    Signed-off-by: ekennedy <evkennedy@upei.ca>

---
+++ js/islandora_newspaper_calendar_page.js
@@ -0,0 +1,17 @@
+Drupal.behaviors.islandora_newspaper_calendar_page = {
+  attach: function(context, settings) {
+    jQuery('#date-selectbox').change(function() {
+      var val = jQuery(this).find('option:selected').attr('value');
+      var path = window.location.href;
+      var loc = path.lastIndexOf('?year=');
+      if (loc >= 0) {
+        var end = loc + 6;
+        var newpath = path.substring(0, end);
+        window.location.href = newpath.concat(val);
+      }
+      else {
+        window.location.href = path.concat("?year=" + val);
+      }
+    });
+  }
+}
